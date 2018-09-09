28b64c509de7900c6b653d2e4c1af3b9573bebe0
BT <37904885+Robuyasu@users.noreply.github.com>
2018-09-09 15:36:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import asyncio
 import twitter
 import requests
 import random
-import commandsdisc
 Client = discord.Client()
 client = commands.Bot(command_prefix='!')
 
@@ -23,12 +22,50 @@ consumer_secret='rcm2pqUj6CMiCmy1TL8PWheimxlJk9CrLcMym569i2zVbIFhba',
 access_token_key='1038495867639136256-Z4Zl3k0vtD3KPe707eDEuCNpcF2geH',
 access_token_secret='e8VDI74qYaXLMxqittastSR3IXDjSjKnCHuTVpvkUjvdm')
 
+async def bootup(message):
+    if message.author.id == RobId:
+        ContextOn = True
+        await client.send_message(message.channel,"Successfully booted up.")
+    else:
+        await client.send_message(message.channel,"You do not have the permissions to do that, %s!" % (message.author.mention))
+
+
+async def bootdown(message):
+    if message.author.id == RobId:
+        ContextOn = False
+        await client.send_message(message.channel,"Successfully booted down.")
+    else:
+        await client.send_message(message.channel,"You do not have the permissions to do that, %s!" % (message.author.mention))
+
+
+async def post(message):
+    if message.author.id == RobId:
+        Content = message.content.split(" ")
+        await client.send_message(message.channel,"Posting message..")
+        TwitApi.PostUpdate(" ".join(Content[1:]))
+        await client.send_message(message.channel,"Posted message to twitter!")
+
+async def version(message):
+    await client.send_message(message.channel,"Version: 1.0.0")
+
+async def about(message):
+    await client.send_message(message.author,'''
+Hey There! I'm No Context Bot.
+I was programmed by @robuyasu#3100, and was created September 9, 2018.
+
+My purpose is to select recent messages, and tweet one of the many random recent messages.
+I am coded in Python, and hosted at heroku.
+
+Want to view my source code or help out? View https://github.com/Robuyasu/noContextBot
+
+    ''')
+
 contcmds = {
-    "!BOOTUP":commandsdisc.bootup,
-    "!BOOTDOWN":commandsdisc.bootdown,
-    "!POST":commandsdisc.post,
-    "!VERSION":commandsdisc.version,
-    "!ABOUT":commandsdisc.about
+    "!BOOTUP":bootup,
+    "!BOOTDOWN":bootdown,
+    "!POST":post,
+    "!VERSION":version,
+    "!ABOUT":about
 }
 
 async def post_tweets():
