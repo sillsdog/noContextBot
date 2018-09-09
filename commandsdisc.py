66697f910220bcd704357173a7e8b85790cae558
BT <37904885+Robuyasu@users.noreply.github.com>
2018-09-09 15:07:21 -0400

    Added commandsdisc module for commands

---
+++ commandsdisc.py
@@ -0,0 +1,37 @@
+def bootup(message):
+    if message.author.id == RobId:
+        ContextOn = True
+        await client.send_message(message.channel,"Successfully booted up.")
+    else:
+        await client.send_message(message.channel,"You do not have the permissions to do that, %s!" % (message.author.mention))
+
+
+def bootdown(message):
+     if message.author.id == RobId:
+        ContextOn = False
+        await client.send_message(message.channel,"Successfully booted down.")
+    else:
+        await client.send_message(message.channel,"You do not have the permissions to do that, %s!" % (message.author.mention))
+
+
+def post(message):
+    if message.author.id == RobId:
+        Content = message.content.split(" ")
+        await client.send_message(message.channel,"Posting message..")
+        TwitApi.PostUpdate(" ".join(Content[1:]))
+        await client.send_message(message.channel,"Posted message to twitter!")
+
+def version(message):
+    await client.send_message(message.channel,"Version: 1.0.0")
+
+def about(message):
+        await client.send_message(message.author,'''
+Hey There! I'm No Context Bot.
+I was programmed by @robuyasu#3100, and was created September 9, 2018.
+
+My purpose is to select recent messages, and tweet one of the many random recent messages.
+I am coded in Python, and hosted at heroku.
+
+Want to view my source code or help out? View https://github.com/Robuyasu/noContextBot
+
+        ''')
