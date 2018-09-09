99816ba44af7c22831beaf01c13e2897080b4f55
BT <37904885+BraelTagsip@users.noreply.github.com>
2018-09-09 12:02:34 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+#No Context Bot by @robuyasu#3100
+#Version 1.0.0
+
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord import Server
+import asyncio
+import twitter
+import requests
+from itertools import cycle
+import random
+
+Client = discord.Client()
+client = commands.Bot(command_prefix='!')
+
+CurrentMessages = []
+ContextOn = True
+
+RobId = "154732271742615553"
+TwitApi = twitter.Api(consumer_key='kEpgtAzIwc3mXuxY8lWpoiMGT',
+consumer_secret='rcm2pqUj6CMiCmy1TL8PWheimxlJk9CrLcMym569i2zVbIFhba',
+access_token_key='1038495867639136256-Z4Zl3k0vtD3KPe707eDEuCNpcF2geH',
+access_token_secret='e8VDI74qYaXLMxqittastSR3IXDjSjKnCHuTVpvkUjvdm')
+
+async def post_tweets():
+    await client.wait_until_ready()
+    await asyncio.sleep(5)
+    while not client.is_closed:
+        if ContextOn and len(CurrentMessages) >= 2:
+            ChosenMsg = random.choice(CurrentMessages)
+            TwitApi.PostUpdate(ChosenMsg.content)
+            await client.send_message(ChosenMsg.channel,"%s, your message has been tweeted to the twitter account!"%(ChosenMsg.author.mention))
+            CurrentMessages.clear()
+        await asyncio.sleep(1800) #Waits for 30 minutes
+
+@client.event
+async def on_ready():
+    print("No Context Bot has been started up. To stop the program @robuyasu#3100 , say !bootdown . Version 1.0.0")
+
+@client.event
+async def on_message(message):
+    if message.channel.id == "488054001795989524":
+        CurrentMessages.append(message)
+
+    if message.content.upper() == "!BOOTDOWN":
+        if message.author.id == RobId:
+            ContextOn = False
+            await client.send_message(message.channel,"Successfully booted down.")
+        else:
+            await client.send_message(message.channel,"You do not have the permissions to do that, %s!" % (message.author.mention))
+
+    elif message.content.upper() == "!BOOTUP":
+        if message.author.id == RobId:
+            ContextOn = True
+            await client.send_message(message.channel,"Successfully booted up.")
+        else:
+            await client.send_message(message.channel,"You do not have the permissions to do that, %s!" % (message.author.mention))
+
+    elif message.content.upper().startswith("!POST"):
+        if message.author.id == RobId:
+            Content = message.content.split(" ")
+            await client.send_message(message.channel,"Posting message..")
+            TwitApi.PostUpdate(" ".join(Content[1:]))
+            await client.send_message(message.channel,"Posted message to twitter!")
+    elif message.content.upper() == "!VERSION":
+        await client.send_message(message.channel,"Version: 1.0.0")
+
+client.loop.create_task(post_tweets())
+client.run('NDg4MTQ0MjUzNjMwMDIxNjUx.DnX8mQ.2l3sgx7QoU1bQAbLTH9LgwQovwI')
