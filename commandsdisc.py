693c35528e4cc32de3a60f166809b32ddeabfeee
BT <37904885+Robuyasu@users.noreply.github.com>
2018-09-09 15:11:44 -0400

    Update commandsdisc.py

--- commandsdisc.py
+++ commandsdisc.py
@@ -18,7 +18,7 @@ consumer_secret='rcm2pqUj6CMiCmy1TL8PWheimxlJk9CrLcMym569i2zVbIFhba',
 access_token_key='1038495867639136256-Z4Zl3k0vtD3KPe707eDEuCNpcF2geH',
 access_token_secret='e8VDI74qYaXLMxqittastSR3IXDjSjKnCHuTVpvkUjvdm')
 
-def bootup(message):
+async def bootup(message):
     if message.author.id == RobId:
         ContextOn = True
         await client.send_message(message.channel,"Successfully booted up.")
@@ -26,7 +26,7 @@ def bootup(message):
         await client.send_message(message.channel,"You do not have the permissions to do that, %s!" % (message.author.mention))
 
 
-def bootdown(message):
+async def bootdown(message):
      if message.author.id == RobId:
         ContextOn = False
         await client.send_message(message.channel,"Successfully booted down.")
@@ -34,17 +34,17 @@ def bootdown(message):
         await client.send_message(message.channel,"You do not have the permissions to do that, %s!" % (message.author.mention))
 
 
-def post(message):
+async def post(message):
     if message.author.id == RobId:
         Content = message.content.split(" ")
         await client.send_message(message.channel,"Posting message..")
         TwitApi.PostUpdate(" ".join(Content[1:]))
         await client.send_message(message.channel,"Posted message to twitter!")
 
-def version(message):
+async def version(message):
     await client.send_message(message.channel,"Version: 1.0.0")
 
-def about(message):
+async def about(message):
         await client.send_message(message.author,'''
 Hey There! I'm No Context Bot.
 I was programmed by @robuyasu#3100, and was created September 9, 2018.
