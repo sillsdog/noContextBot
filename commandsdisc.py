76f8ca07e4daaab461e74dfdf2101734d93e43f3
BT <37904885+Robuyasu@users.noreply.github.com>
2018-09-09 15:13:21 -0400

    Update commandsdisc.py

--- commandsdisc.py
+++ commandsdisc.py
@@ -27,11 +27,11 @@ async def bootup(message):
 
 
 async def bootdown(message):
-     if message.author.id == RobId:
-        ContextOn = False
-        await client.send_message(message.channel,"Successfully booted down.")
+    if message.author.id == RobId:
+    ContextOn = False
+    await client.send_message(message.channel,"Successfully booted down.")
     else:
-        await client.send_message(message.channel,"You do not have the permissions to do that, %s!" % (message.author.mention))
+    await client.send_message(message.channel,"You do not have the permissions to do that, %s!" % (message.author.mention))
 
 
 async def post(message):
@@ -45,7 +45,7 @@ async def version(message):
     await client.send_message(message.channel,"Version: 1.0.0")
 
 async def about(message):
-        await client.send_message(message.author,'''
+    await client.send_message(message.author,'''
 Hey There! I'm No Context Bot.
 I was programmed by @robuyasu#3100, and was created September 9, 2018.
 
@@ -54,4 +54,4 @@ I am coded in Python, and hosted at heroku.
 
 Want to view my source code or help out? View https://github.com/Robuyasu/noContextBot
 
-        ''')
+    ''')
