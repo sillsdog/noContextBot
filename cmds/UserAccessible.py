5012fd6d951dc12f97504cbf08a27681c7214f49
BT <37904885+Robuyasu@users.noreply.github.com>
2018-09-10 02:55:19 -0400

    Add files via upload

---
+++ cmds/UserAccessible.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class UserAccessible: 
+    @client.command(pass_context=True)
+    async def version(self,ctx):
+        await client.say("Version: " + CurrentVersion)
+
+    @client.command(pass_context=True)
+    async def about(self,ctx):
+        message = ctx.message
+        await client.send_message(message.author,open('text/discordhelp.txt').read())
+
+def setup(bot):
+    bot.add_cog(UserAccessible(bot))
