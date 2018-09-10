65891cbfe412c52822be6015ce777834aedb6517
BT <37904885+Robuyasu@users.noreply.github.com>
2018-09-10 07:26:05 -0400

    Create ModeratorOnly.py

---
+++ cmds/ModeratorOnly.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import asyncio
+import twitter
+from TwitApi import TwitApi
+modroles = open("text/modroles.txt").read()
+
+def IsMod(user):
+    for role in user.roles:
+        if modroles.find(role.name):
+            return True
+    return False
+
+class ModeratorOnly:
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(pass_context=True)
+    async def kick(self, ctx, member: discord.Member):
+        if IsMod(ctx.author):
+            await self.client.kick(member)
+            await self.client.send(member.mention + " has been kicked.")
+
+    @commands.command(pass_context=True)
+    async def ban(self, ctx, member: discord.Member, days: int = 1):
+        if IsMod(ctx.author):
+            await self.client.ban(member,days)
+            await self.client.send("%s has been banned for %s days." % (member.mention,str(days)) )    
