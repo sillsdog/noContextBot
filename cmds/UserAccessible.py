5155518f8ba6009a305edac84f7c9fbeb58038e4
BT <37904885+Robuyasu@users.noreply.github.com>
2018-09-10 03:13:11 -0400

    Update UserAccessible.py

--- cmds/UserAccessible.py
+++ cmds/UserAccessible.py
@@ -5,11 +5,11 @@ class UserAccessible:
     def __init__(self,client):
         self.client = client
 
-    @client.command(pass_context=True)
+    @commands.command(pass_context=True)
     async def version(self,ctx):
         await ctx.send("Version: " + CurrentVersion)
 
-    @client.command(pass_context=True)
+    @commands.command(pass_context=True)
     async def about(self,ctx):
         message = ctx.message
         await client.send_message(message.author,open('text/discordhelp.txt').read())
