36669a7c478265b85c280948b840bc0fa47f543c
BT <37904885+Robuyasu@users.noreply.github.com>
2018-09-10 03:00:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ initial_extensions = [
 if __name__ == '__main__':
     for extension in initial_extensions:
         try:
-            bot.load_extension(extension)
+            client.load_extension(extension)
         except Exception as e:
             print(f'Failed to load extension {extension}.', file=sys.stderr)
             traceback.print_exc()
